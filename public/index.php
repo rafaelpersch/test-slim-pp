3f27155882ebd437cc3e4a2feb99290c9f93f2ed
rafael persch <rafaelpersch@hotmail.com>
2023-02-01 00:27:37 -0300

    models

--- public/index.php
+++ public/index.php
@@ -1,8 +1,10 @@
 <?php
+
+declare(strict_types=1);
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
-use App\Model;
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../src/config.php';
@@ -26,7 +28,12 @@ $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response, $args) {
 
-    //$cc = new Tarefa();
+    /*$tarefa = new App\Models\Tarefa();
+    $tarefa->id = 10;
+    $tarefa->descricao = 'nome';
+    $tarefa->observacao = 'obs de boas';
+
+    var_dump($tarefa);*/
 
     $response->getBody()->write("Hello world!");
     return $response;
---
+++ src/Models/Tarefa.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+class Tarefa {
+    public int $id;
+    public string $descricao;
+    public string $observacao;
+}
