6e4b2c71d2386939ab5a6ad67192c2a715c0cf2b
rafael persch <rafaelpersch@hotmail.com>
2023-02-01 22:34:38 -0300

    model usuario

---
+++ src/Models/Usuario.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+class Usuario {
+    public int $id;
+    public string $nome;
+    public string $email;
+    public string $senha;
+}
