47749b46cc6587e8d0f53ef5e3c7e9c86ed844e1
rafael persch <rafaelpersch@hotmail.com>
2023-02-10 00:00:43 -0300

    template

--- public/index.php
+++ public/index.php
@@ -5,15 +5,31 @@ declare(strict_types=1);
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../src/config.php';
 
+// Create App   
 $app = AppFactory::create();
+
+// Create Twig
+$twig = Twig::create(__DIR__ . '/../Templates');
+
+// Add Twig-View Middleware
+$app->add(TwigMiddleware::create($app, $twig));
 
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello world!");
     return $response;
+});
+
+$app->get('/render', function (Request $request, Response $response, $args) {
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'hello.html', [
+        'name' => 'joaooasdsdfasdoo'
+    ]);
 });
 
 $app->get('/teste', function (Request $request, Response $response, $args) {
